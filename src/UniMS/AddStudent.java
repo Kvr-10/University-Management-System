ae9ad9b29c90a15557835af093c028abb3b59546
Chaman <chamansinha9480@gmail.com>
2024-08-13 03:45:09 +0530

    Version 1.0

--- src/UniMS/AddStudent.java
+++ src/UniMS/AddStudent.java
@@ -20,11 +20,13 @@ public class AddStudent extends JFrame implements ActionListener {
 
     public AddStudent(){
         setBounds(250,30,800,650);
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Icon/qwe.jpeg"));
-        Image i2 = i1.getImage().getScaledInstance(800,650,Image.SCALE_DEFAULT);
-        ImageIcon i3= new ImageIcon(i2);
-        JLabel image = new JLabel(i3);
-        image.setBounds(0,0,800,650);
+
+//        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Icon/qwe.jpeg"));
+//        Image i2 = i1.getImage().getScaledInstance(800,650,Image.SCALE_DEFAULT);
+//        ImageIcon i3= new ImageIcon(i2);
+//        JLabel image = new JLabel(i3);
+//        image.setBounds(0,0,800,650);
+
         getContentPane().setBackground(Color.white);
         setLayout(null);
 
@@ -163,7 +165,7 @@ public class AddStudent extends JFrame implements ActionListener {
         Back.addActionListener(this);
         add(Back);
 
-        add(image);
+        //add(image);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         getRootPane().setDefaultButton(Submit);
